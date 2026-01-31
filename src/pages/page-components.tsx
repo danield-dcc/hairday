2533c762c148d1d664d5d04e2f8aed0264680693
Daniel <danieldcc.colares@gmail.com>
2026-01-31 09:59:23 -0300

    adiciona componente icons

--- src/pages/page-components.tsx
+++ src/pages/page-components.tsx
@@ -1,5 +1,15 @@
 import Button from "../components/button";
+import Icon from "../components/icon";
 import Text from "../components/text";
+import CalendarIcon from "../assets/icons/calendar.svg?react";
+import CaretDownIcon from "../assets/icons/caretDown.svg?react";
+import CaretLeftIcon from "../assets/icons/caretLeft.svg?react";
+import CaretRightIcon from "../assets/icons/caretRight.svg?react";
+import CloudSunIcon from "../assets/icons/cloudSun.svg?react";
+import MoonStarsIcon from "../assets/icons/moonStars.svg?react";
+import SunHorizonIcon from "../assets/icons/sunHorizon.svg?react";
+import TrashIcon from "../assets/icons/trash.svg?react";
+import UserIcon from "../assets/icons/user.svg?react";
 
 export default function PageComponents() {
   return (
@@ -8,10 +18,34 @@ export default function PageComponents() {
         <Text variant="title-lg" className="text-yellow-light">
           Olá mundo!
         </Text>
+        <Text variant="title-md" className="text-yellow-light">
+          Olá mundo!
+        </Text>
+        <Text variant="title-sm" className="text-yellow-light">
+          Olá mundo!
+        </Text>
+        <Text variant="body-md" className="text-yellow-light">
+          Olá mundo!
+        </Text>
+        <Text variant="body-sm" className="text-yellow-light">
+          Olá mundo!
+        </Text>
       </div>
       <div className="flex flex-col gap-2">
         <Button>AGENDAR</Button>
         <Button disabled>AGENDAR</Button>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={CalendarIcon} className="fill-gray-300" />
+        <Icon svg={CaretDownIcon} />
+        <Icon svg={CaretLeftIcon} />
+        <Icon svg={CaretRightIcon} />
+        <Icon svg={CloudSunIcon} />
+        <Icon svg={MoonStarsIcon} />
+        <Icon svg={SunHorizonIcon} />
+        <Icon svg={TrashIcon} />
+        <Icon svg={UserIcon} />
       </div>
     </div>
   );
