9d6bccf0524375030527f40a68ea6236e9aef258
Daniel <danieldcc.colares@gmail.com>
2026-02-02 16:56:16 -0300

    adiciona componente Container

---
+++ src/components/container.tsx
@@ -0,0 +1,34 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { createElement, type ComponentProps, type JSX } from "react";
+
+export const ContainerVariants = cva("mx-auto", {
+  variants: {
+    size: {
+      md: "max-w-360 px-2",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
+
+interface ContainerProps
+  extends VariantProps<typeof ContainerVariants>, ComponentProps<"div"> {
+  as?: keyof JSX.IntrinsicElements;
+}
+
+export default function Container({
+  as = "div",
+  className,
+  children,
+  ...props
+}: ContainerProps) {
+  return createElement(
+    as,
+    {
+      className: ContainerVariants({ size: "md", className }),
+      ...props,
+    },
+    children,
+  );
+}
